66fd12e3cb9b8c7a2232dd233dd130b6ab60fff7
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2023-08-07 15:07:49 -0400

    async_function

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
+'''
+    The basics of async.
+'''
 
-import random
 import asyncio
+import random
 
-async def wait_random(max_delay=10)->float:
-    """ asynchronous an integer argument (max_delay, with a default value of 10)
-        named wait_random that waits for a random delay between 0 and max_delay 
-        (included and float value) seconds and eventually returns it.
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    waits for a random delay between 0 and max_delay (included and float value)
+    seconds and eventually returns it.
     """
     delay = random.uniform(0, max_delay)
-    await asyncio.sleep(0, max_delay)
+    await asyncio.sleep(delay)
     return delay
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """ The basics of async """
+
+import asyncio
+from typing import List
+
 wait_random = __import__('0-basic_async_syntax').wait_random
-import asyncio 
-async def wait_n(n, max_delay):
-    delays = []
-    tasks = []
-    for _ in range(n):
-        task = asyncio.create_task(wait_random(max_delay))
-        tasks.append(task)
-    results = await asyncio.gather(*tasks)
-    for result in results:
-        delays.append(result)
-    return sorted(delays)
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    spawn wait_random n times with the specified max_delay.
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
